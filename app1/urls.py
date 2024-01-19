605fd59b89d6e4ab4a4cea9b294f2affa8c715e4
LIJI-VIJAYAN <lijivijayan93@gmail.com>
2024-01-19 23:55:33 +0530

    style update

--- app1/urls.py
+++ app1/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',views.home,name='home'),
+    path('',views.store,name='store'),
     path('home/',views.home,name='home'),
     path('register/',views.register,name='register'),
     path('login/',views.login,name='login'),
